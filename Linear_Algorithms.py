5f5ae9a4db26f527f71e442c852e4189111a5fac
SabienNguyen <sabien.q.nguyen@gmail.com>
2024-05-15 00:05:43 -0700

    add code to show usage of simple linear regression and finish simple linear regression implementation

--- Linear_Algorithms.py
+++ Linear_Algorithms.py
@@ -1,2 +1,15 @@
 #TODO create linear regression function
 #TODO create logistic regression function
+import numpy as np
+
+def SimpleLinearRegression(X, y) :
+    m = 0 
+    b = 0
+    
+    X_bar = np.mean(X)
+    y_bar = np.mean(y)
+    
+    m = np.sum((X - X_bar) * (y - y_bar)) / np.sum((X - X_bar) ** 2)
+    b = y_bar - m * X_bar
+    
+    return (m, b)
