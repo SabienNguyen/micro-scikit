ac1feae10d10c72ddf05f5734d5eee2cc091abd7
SabienNguyen <sabien.q.nguyen@gmail.com>
2025-05-02 08:04:09 -0700

    add training loop, implement gradient descent

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -9,6 +9,8 @@ import numpy as np
 class Logistic_Regression:
     
     def __init__(self):
+        self.weights = 0
+        self.bias = 0
         return
     
     def _sigmoid(self, z):
@@ -17,23 +19,35 @@ class Logistic_Regression:
     def _log_loss(self, y, y_hat):
         return -1 * (y * np.log(y_hat) + (1-y) * np.log(1 - y_hat))
     
+    def _gradient(self):
+        return
+    
     def train(self, X, y, alpha):
         # steps
         # figure out stopping point (when we hit gradient)
         # initialize weights and bias
-        weights = np.zeros(X.shape[1])
-        b = 0
         # training loop
+        prev_loss = 0
+        while True:
+            self.weights = np.zeros(X.shape[1])
+            loss = 0
+            for i, (_, row) in enumerate(X.iterrows()):
+                y_hat = self.predict(input)
+                loss += self._log_loss(y[i], y_hat)
+            loss /= X.shape[0]
+            if np.abs(loss - prev_loss) < 0.0001 :
+                break
+            prev_loss = loss
         # need to define a loss function to measure performance using log loss
         # take linear combination of training data and weights
         # predict on all inputs
         # find loss
         # adjust weights accordingly
-        pass
+        return
     
-    def compute_linear_combination(self, weights, input, bias):
-        return np.dot(weights, input) + bias
+    def _compute_linear_combination(self, input):
+        return np.dot(self.weights, input) + self.bias
     
     def predict(self, input):
-        return 1 if self.sigmoid(self.compute_linear_combination(input)) > 0.5 else 0
+        return 1 if self.sigmoid(self._compute_linear_combination(input)) > 0.5 else 0
     
