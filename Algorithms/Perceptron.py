59c1df6cd121cdd62a61878ef1da634ceb4d886a
Sabien Nguyen <sabien.q.nguyen@gmail.com>
2025-05-19 20:07:28 -0700

    start perceptron class

---
+++ Algorithms/Perceptron.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class Perceptron:
+    
+    def __init__(self):
+        pass
+    
+    def activation(self):
+        pass
+    
+    def forward(self):
+        pass
+    
+    def train(self):
+        pass
+    
+    def predict(self):
+        pass
+    
